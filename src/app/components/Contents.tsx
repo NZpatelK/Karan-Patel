0323f434c3554ea1c9d5d9a5b9f62d21ab49d239
NZpatelK <patelk.dev@hotmail.com>
2024-03-14 11:36:46 +1300

    implement styles

--- src/app/components/Contents.tsx
+++ src/app/components/Contents.tsx
@@ -4,22 +4,23 @@ import '../styles/Contents.css';
 const Contents: React.FC = () => {
     const sections = [
         { title: 'Introduction', id: 'introduction', page: 1 },
-        { title: 'Chapter 1', id: 'chapter-1', page: 5 },
-        { title: 'Chapter 2', id: 'chapter-2', page: 10 },
-        { title: 'Conclusion', id: 'conclusion', page: 15 },
+        { title: 'About Me', id: 'about-me', page: 5 },
+        { title: 'Skills', id: 'skills', page: 10 },
+        { title: 'Services', id: 'services', page: 15 },
+        { title: 'Projects', id: 'projects', page: 20 },
+        { title: 'Contact Me', id: 'contact ', page: 25 },
     ];
 
     return (
         <div className='contents-section'>
             <h2>Table of Contents</h2>
-            <ol style={{ listStyle: 'none', paddingLeft: 0 }}>
+            <ol className='list-contents'>
                 {sections.map((section) => (
                     <li key={section.id} style={{ display: 'flex', justifyContent: 'space-between' }}>
                         <div style={{ marginRight: '10px' }}>
                             <a href={`#${section.id}`}>{section.title}</a>
                         </div>
                         <div style={{ marginLeft: 'auto', fontStyle: 'italic' }}>
-                            <span style={{ marginRight: '5px' }}>.....</span>
                             <span>{section.page}</span>
                         </div>
                     </li>
