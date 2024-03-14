bd6308accac1dc023761e82bb9663075062d18aa
NZpatelK <patelk.dev@hotmail.com>
2024-03-14 15:09:26 +1300

    Update AboutMe.tsx

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -8,17 +8,19 @@ type AboutMeProps = {
     id: string;
 };
 
-const AboutMe: React.FC<AboutMeProps> = ({id, turnPage}) => {
+const AboutMe: React.FC<AboutMeProps> = ({ id, turnPage }) => {
     return (
         <div className='about-me-section'>
             <h2>About Me</h2>
             <p>
-                I&apos;m a recent graduate from AUT, holding a Bachelor of Computer and Information Science with a focus on Software Development. With internships at Kara Technologies and UiRev, 
-                I&apos;ve gained practical experience in database technology and junior development roles. My passion lies in front-end, web, and mobile development, along with UI/UX design, 
-                where I thrive on creating visually captivating user experiences. Beyond coding, I enjoy hiking, watching movies, and exploring diverse cuisines. 
-                I&apos;m an open-minded individual who eagerly embraces new challenges and opportunities for learning, with a strong drive for personal and professional growth.
+                I&apos;m a recent graduate from AUT with a Bachelor&apos;s degree in Computer and Information Science,
+                specializing in software development. Through internships at Kara Technologies and UiRev,
+                I&apos;ve honed my skills as a Database Technician and Junior Developer. My passion lies in frontend, web,
+                and mobile development, coupled with UI/UX design. Beyond tech, I enjoy hiking, watching movies, and exploring diverse cuisines.
+                With an open mind and a hunger for learning,
+                I thrive on new challenges and consider myself a fast learner, always eager to grow personally and professionally.
             </p>
-            <FontAwesomeIcon className='prev page-btn' icon={faCaretRight} onClick={(e) => { e.preventDefault() ,turnPage(id)}}/>
+            <FontAwesomeIcon className='prev page-btn' icon={faCaretRight} onClick={(e) => { e.preventDefault(), turnPage(id) }} />
         </div>
     );
 };
