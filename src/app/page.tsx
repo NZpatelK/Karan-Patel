e4715944de82a8535ffbd1b09cf91f745ab7b3b2
NZpatelK <patelk.dev@hotmail.com>
2024-03-14 22:29:36 +1300

    add  programming tools logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Services from "./components/Services";
 import Projects from "./components/Projects";
 import ContactUs from "./components/ContactUs";
 
-
 export default function Home() {
 
   const handleSelectContent = ((pageId: number) => {
