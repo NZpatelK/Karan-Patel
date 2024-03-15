fc9307a0809fcb2af53842ff4761f63d1fb2d448
NZpatelK <patelk.dev@hotmail.com>
2024-03-15 16:58:51 +1300

    add icon

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -4,12 +4,12 @@ import '../styles/Services.css';
 const Services: React.FC = () => {
     return (
         <div className='services-section'>
-            <h1>Service Offers</h1>
-            <ul>
+            <h1>Service</h1>
+            <div>
                 <li>Web Development</li>
                 <li>Mobile Development</li>
                 <li>UI Design</li>
-            </ul>
+            </div>
         </div>
     );
 };
