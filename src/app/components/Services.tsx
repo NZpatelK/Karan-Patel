babf5d0ac9fdffdc6486433761e6bb0ff0321a58
NZpatelK <patelk.dev@hotmail.com>
2024-03-15 17:36:02 +1300

    implement service

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import '../styles/Services.css';
+import webDev from '../assets/webDev.png';
+import mobileDev from '../assets/mobileDev.png';
+import uiDesigner from '../assets/uiDesign.png';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
-const Services: React.FC = () => {
+type ServiceProps = {
+    turnPage: (value: string) => void;
+    id: string;
+};
+
+const Services: React.FC<ServiceProps> = ({id, turnPage }) => {
     return (
         <div className='services-section'>
             <h1>Service</h1>
-            <div>
-                <li>Web Development</li>
-                <li>Mobile Development</li>
-                <li>UI Design</li>
+
+            <div className='serviceContainer'>
+                <div className='serviceGroup'>
+                    <div className="serviceType">
+                        <Image className='serviceIcon' src={webDev} alt="web development" />
+                        <h2>Web Development</h2>
+                    </div>
+                    <div className="serviceType">
+                        <Image className='serviceIcon' src={mobileDev} alt="web development" />
+                        <h2>Mobile Development</h2>
+                    </div>
+                    <div className="serviceType">
+                        <Image className='serviceIcon' src={uiDesigner} alt="web development" />
+                        <h2>UI/UX Designer</h2>
+                    </div>
+                </div>
+
+                <p>
+                    I am a full stack developer with a passion for creating and designing web and mobile applications.
+                    I am also a UI/UX designer with a passion for creating and designing user interfaces and user experiences.
+                </p>
             </div>
+
+            <FontAwesomeIcon className='prev page-btn' icon={faCaretRight} onClick={(e) => { e.preventDefault(), turnPage(id) }} />
+
         </div>
     );
 };
