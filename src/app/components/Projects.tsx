3a2a95dca07ee333eee46295db4cefabbb54c509
NZpatelK <patelk.dev@hotmail.com>
2024-03-23 11:38:59 +1300

    update the project desc content

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
-const Projects: React.FC= () => {
+const Projects: React.FC = () => {
 
     return (
         <div className='projects-section'>
@@ -14,10 +14,14 @@ const Projects: React.FC= () => {
                 <h3 className='title'>Latest Project</h3>
                 <Image className='projectImg' src={latestProject} alt='latest project' />
                 <div className='project-title-and-link'>
-                    <h3>Nutrition Website</h3> 
-                    <a className='latest-project-link' href='https://nzpatelk.github.io/Nutrition-Ecomm/'><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{color: "#0055ff",}} /> Preview </a>
+                    <h3>Nutrition Website</h3>
+                    <a className='latest-project-link' href='https://nzpatelk.github.io/Nutrition-Ecomm/'><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{ color: "#0055ff", }} /> Preview </a>
                 </div>
-                <p className='project_desc'>Project Description</p>
+                <p className='project_desc'>
+                    Welcome to the Nutrition E-commerce website, a showcase of my expertise in crafting exceptional user interfaces. 
+                    Through meticulous design and innovative features, I&apos;ve created a dynamic platform that seamlessly integrates captivating animations and transitions.
+                    Feel free to click the preview link below to experience the website firsthand and immerse yourself in its interactive elements.
+                </p>
             </div>
             <a className='linkGithub' href='https://github.com/NZpatelK?tab=repositories'>View All Projects</a>
         </div>
